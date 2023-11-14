d85b31c1136c3cf48951a6332c306c40ee8da140
giwon512 <giwon512@naver.com>
2023-11-15 01:14:02 +0900

    enum 클래스 수정 및 프로퍼티 리턴 함수 제작

--- src/main/java/domain/Badge.java
+++ src/main/java/domain/Badge.java
@@ -1,5 +1,17 @@
 package domain;
 
 public enum Badge {
-    별, 트리, 산타
+    STAR("별"),
+    TREE("트리"),
+    SANTA("산타");
+
+    private final String name;
+
+    private Badge(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/main/java/domain/Menu.java
+++ src/main/java/domain/Menu.java
@@ -1,17 +1,23 @@
 package domain;
 
 public enum Menu {
-    양송이수프(6000), 타파스(5500), 시저샐러드(8000),
-    티본스테이크(55000), 바비큐립(54000), 해산물파스타(35000), 크리스마스파스타(25000),
-    초코케이크(15000), 아이스크림(5000),
-    제로콜라(3000), 레드와인(60000), 샴페인(25000);
+    APPETIZER1("양송이수프", 6000), APPETIZER2("타파스", 5500), APPETIZER3("시저샐러드", 8000),
+    MAIN1("티본스테이크", 55000), MAIN2("바비큐립", 54000), MAIN3("해산물파스타", 35000), MAIN4("크리스마스파스타", 25000),
+    DESSERT1("초코케이크", 15000), DESSERT2("아이스크림", 5000),
+    BEVERAGE1("제로콜라", 3000), BEVERAGE2("레드와인", 60000), BEVERAGE3("샴페인", 25000);
 
-    private int price;
-    private Menu(int price){
+    private final int price;
+    private final String name;
+    private Menu(String name, int price){
+        this.name = name;
         this.price = price;
     }
 
     public int getPrice(){
         return price;
+    }
+
+    public String getName() {
+        return name;
     }
 }
